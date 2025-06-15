c957cd0fb3fea4298a74b3724cd35af0fe768c39
Jonan Osorio <jonanosorio20@gmail.com>
2025-06-15 10:01:15 -0400

    Submitting my progress

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -10,7 +10,35 @@ export function AuthProvider({ children }) {
 
   // TODO: signup
 
+  const signup = async (credentials) => {
+    try {
+      const ressponse = await fetch(`${API}/signup`, {
+      method: "POST",
+      header: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),  
+      });
+      const result = await Response.json();
+      setToken(result.token);
+      setLocation("TABLET");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   // TODO: authenticate
+
+const authenticate = async () => {
+  try {
+    if (!token) throw Error("No token found");
+    const response = await fetch (`${API}/authenticate`, {
+      headers: { Authrization: `bearer ${token}`},
+  });
+  if (!response.ok) throw Error("Authentication failed");
+  setLocation("TUNNEL");
+  } catch (error) {
+    console.error(error);
+  }
+}
 
   const value = { location };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
